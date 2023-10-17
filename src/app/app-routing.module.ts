0dc113563ad0d952641f35add43ffb7bfd7d2ede
ABINASH GOUDA <Abinash.Gouda01@futuregenerali.in>
2023-10-17 16:02:10 +0530

    added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+
+
+const routes: Routes = [
+  {
+    path:'**',component:BasicFormComponent
+  },
+  {
+    path:'reister',component:BasicFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
