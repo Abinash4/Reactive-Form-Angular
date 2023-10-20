aaab2220700408d258339cfd0a49a3843702b63a
ABINASH GOUDA <Abinash.Gouda01@futuregenerali.in>
2023-10-20 19:57:04 +0530

    added material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BasicFormComponent } from './basic-form/basic-form.component';
+import { MaterialFormComponent } from './material-form/material-form.component';
 
 
 const routes: Routes = [
   {
-    path:'**',component:BasicFormComponent
-  },
-  {
     path:'reister',component:BasicFormComponent
   },
   {
-    path:'reister',component:BasicFormComponent
-  }
+    path:'matReister',component:MaterialFormComponent
+  },
+  {
+    path:'',component:BasicFormComponent
+  },
 ];
 
 @NgModule({
